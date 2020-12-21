fef97b7aafddfe3895dc2362738fb21318ccd10d
李文威 <1061678010@qq.com>
2020-12-22 01:34:02 +0800

    Add files via upload

---
+++ lab5/Exe1&2&3/filesys/directory.cc
@@ -0,0 +1,237 @@
+// directory.cc 
+//	Routines to manage a directory of file names.
+//
+//	The directory is a table of fixed length entries; each
+//	entry represents a single file, and contains the file name,
+//	and the location of the file header on disk.  The fixed size
+//	of each directory entry means that we have the restriction
+//	of a fixed maximum size for file names.
+//
+//	The constructor initializes an empty directory of a certain size;
+//	we use ReadFrom/WriteBack to fetch the contents of the directory
+//	from disk, and to write back any modifications back to disk.
+//
+//	Also, this implementation has the restriction that the size
+//	of the directory cannot expand.  In other words, once all the
+//	entries in the directory are used, no more files can be created.
+//	Fixing this is one of the parts to the assignment.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "utility.h"
+#include "filehdr.h"
+#include "directory.h"
+
+//----------------------------------------------------------------------
+// Directory::Directory
+// 	Initialize a directory; initially, the directory is completely
+//	empty.  If the disk is being formatted, an empty directory
+//	is all we need, but otherwise, we need to call FetchFrom in order
+//	to initialize it from disk.
+//
+//	"size" is the number of entries in the directory
+//----------------------------------------------------------------------
+
+Directory::Directory(int size)
+{
+    table = new DirectoryEntry[size];
+    tableSize = size;
+    nameFileHdrSector = -1;
+    for (int i = 0; i < tableSize; i++)
+	    table[i].inUse = FALSE;
+}
+
+//----------------------------------------------------------------------
+// Directory::~Directory
+// 	De-allocate directory data structure.
+//----------------------------------------------------------------------
+
+Directory::~Directory()
+{ 
+    delete [] table;
+} 
+
+//----------------------------------------------------------------------
+// Directory::FetchFrom
+// 	Read the contents of the directory from disk.
+//
+//	"file" -- file containing the directory contents
+//----------------------------------------------------------------------
+
+void
+Directory::FetchFrom(OpenFile *file)
+{
+    (void) file->ReadAt((char *)table, tableSize * sizeof(DirectoryEntry), 0);
+    (void) file->ReadAt((char *)&nameFileHdrSector, sizeof(int), tableSize * sizeof(DirectoryEntry));
+    (void) file->ReadAt((char *)&nameFilePosition, sizeof(int), tableSize * sizeof(DirectoryEntry) + sizeof(int));
+}
+
+//----------------------------------------------------------------------
+// Directory::WriteBack
+// 	Write any modifications to the directory back to disk
+//
+//	"file" -- file to contain the new directory contents
+//----------------------------------------------------------------------
+
+void
+Directory::WriteBack(OpenFile *file)
+{
+    (void) file->WriteAt((char *)table, tableSize * sizeof(DirectoryEntry), 0);
+    (void) file->WriteAt((char *)&nameFileHdrSector, sizeof(int), tableSize * sizeof(DirectoryEntry));
+    (void) file->WriteAt((char *)&nameFilePosition, sizeof(int), tableSize * sizeof(DirectoryEntry) + sizeof(int));
+}
+
+//----------------------------------------------------------------------
+// Directory::FindIndex
+// 	Look up file name in directory, and return its location in the table of
+//	directory entries.  Return -1 if the name isn't in the directory.
+//
+//	"name" -- the file name to look up
+//----------------------------------------------------------------------
+
+int
+Directory::FindIndex(char *name)
+{
+    char *nameBuffer;
+    ASSERT(nameFileHdrSector != -1);
+    OpenFile *nameFile = new OpenFile(nameFileHdrSector);
+    for (int i = 0; i < tableSize; i++)
+        if (table[i].inUse) {
+            nameBuffer = new char[table[i].nameLength];
+            nameFile->ReadAt(nameBuffer, table[i].nameLength, table[i].namePosition);
+            if (!strncmp(nameBuffer, name, table[i].nameLength)){
+                delete nameBuffer;
+                delete nameFile;
+	            return i;
+            }
+            delete nameBuffer;
+        }
+    delete nameFile;
+    return -1;		// name not in directory
+}
+
+//----------------------------------------------------------------------
+// Directory::Find
+// 	Look up file name in directory, and return the disk sector number
+//	where the file's header is stored. Return -1 if the name isn't 
+//	in the directory.
+//
+//	"name" -- the file name to look up
+//----------------------------------------------------------------------
+
+int
+Directory::Find(char *name)
+{
+    int i = FindIndex(name);
+
+    if (i != -1)
+	    return table[i].sector;
+    return -1;
+}
+
+//----------------------------------------------------------------------
+// Directory::Add
+// 	Add a file into the directory.  Return TRUE if successful;
+//	return FALSE if the file name is already in the directory, or if
+//	the directory is completely full, and has no more space for
+//	additional file names.
+//
+//	"name" -- the name of the file being added
+//	"newSector" -- the disk sector containing the added file's header
+//----------------------------------------------------------------------
+
+bool
+Directory::Add(char *name, int newSector)
+{
+    if (FindIndex(name) != -1)
+	    return FALSE;
+    ASSERT(nameFileHdrSector != -1);
+    OpenFile *nameFile = new OpenFile(nameFileHdrSector);
+    int len = strlen(name) + 1;
+
+    for (int i = 0; i < tableSize; i++)
+        if (!table[i].inUse) {
+            table[i].inUse = TRUE;
+            table[i].namePosition = nameFilePosition;
+            table[i].nameLength = len;
+            nameFile->WriteAt(name, len, nameFilePosition);
+            nameFilePosition += len;
+            table[i].sector = newSector;
+            delete nameFile;
+            return TRUE;
+        }
+    delete nameFile;
+    return FALSE;	// no space.  Fix when we have extensible files.
+}
+
+//----------------------------------------------------------------------
+// Directory::Remove
+// 	Remove a file name from the directory.  Return TRUE if successful;
+//	return FALSE if the file isn't in the directory. 
+//
+//	"name" -- the file name to be removed
+//----------------------------------------------------------------------
+
+bool
+Directory::Remove(char *name)
+{ 
+    int i = FindIndex(name);
+
+    if (i == -1)
+	return FALSE; 		// name not in directory
+    table[i].inUse = FALSE;
+    return TRUE;	
+}
+
+//----------------------------------------------------------------------
+// Directory::List
+// 	List all the file names in the directory. 
+//----------------------------------------------------------------------
+
+void
+Directory::List()
+{
+    char *nameBuffer;
+    ASSERT(nameFileHdrSector != -1);
+    OpenFile *nameFile = new OpenFile(nameFileHdrSector);
+    for (int i = 0; i < tableSize; i++)
+	    if (table[i].inUse) {
+            nameBuffer = new char[table[i].nameLength];
+            nameFile->ReadAt(nameBuffer, table[i].nameLength, table[i].namePosition);
+	        printf("%s\n", nameBuffer);
+            delete nameBuffer;
+        }
+    delete nameFile;
+}
+
+//----------------------------------------------------------------------
+// Directory::Print
+// 	List all the file names in the directory, their FileHeader locations,
+//	and the contents of each file.  For debugging.
+//----------------------------------------------------------------------
+
+void
+Directory::Print()
+{ 
+    char *nameBuffer;
+    ASSERT(nameFileHdrSector != -1);
+    OpenFile *nameFile = new OpenFile(nameFileHdrSector);
+    FileHeader *hdr = new FileHeader;
+
+    printf("Directory contents:\n");
+    for (int i = 0; i < tableSize; i++)
+	if (table[i].inUse) {
+        nameBuffer = new char[table[i].nameLength];
+        nameFile->ReadAt(nameBuffer, table[i].nameLength, table[i].namePosition);
+	    printf("Name: %s, Sector: %d\n", nameBuffer, table[i].sector);
+	    hdr->FetchFrom(table[i].sector);
+	    hdr->Print();
+        delete nameBuffer;
+	}
+    printf("\n");
+    delete hdr;
+    delete nameFile;
+}
---
+++ lab5/Exe1&2&3/filesys/filehdr.cc
@@ -0,0 +1,307 @@
+// filehdr.cc 
+//	Routines for managing the disk file header (in UNIX, this
+//	would be called the i-node).
+//
+//	The file header is used to locate where on disk the 
+//	file's data is stored.  We implement this as a fixed size
+//	table of pointers -- each entry in the table points to the 
+//	disk sector containing that portion of the file data
+//	(in other words, there are no indirect or doubly indirect 
+//	blocks). The table size is chosen so that the file header
+//	will be just big enough to fit in one disk sector, 
+//
+//      Unlike in a real system, we do not keep track of file permissions, 
+//	ownership, last modification date, etc., in the file header. 
+//
+//	A file header can be initialized in two ways:
+//	   for a new file, by modifying the in-memory data structure
+//	     to point to the newly allocated data blocks
+//	   for a file already on disk, by reading the file header from disk
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+
+#include "system.h"
+#include "filehdr.h"
+#include <time.h>
+#include "openfile.h"
+
+//----------------------------------------------------------------------
+// FileHeader::Allocate
+// 	Initialize a fresh file header for a newly created file.
+//	Allocate data blocks for the file out of the map of free disk blocks.
+//	Return FALSE if there are not enough free blocks to accomodate
+//	the new file.
+//
+//	"freeMap" is the bit map of free disk sectors
+//	"fileSize" is the bit map of free disk sectors
+//----------------------------------------------------------------------
+
+
+
+bool
+FileHeader::Allocate(BitMap *freeMap, int fileSize, fileType FileType)
+{ 
+    numBytes = fileSize;
+    type = FileType;
+    numSectors  = divRoundUp(fileSize, SectorSize);
+
+    int unassignedSectors = numSectors;
+    if (unassignedSectors > NumDirect) {
+        if (freeMap->NumClear() < NumDirect)
+            return FALSE;
+        for (int i = 0; i < NumDirect; i++)
+            dataSectors[i] = freeMap->Find();
+        
+        dataSectors[NumDirect] = freeMap->Find();
+        if (dataSectors[NumDirect] == -1)
+            return FALSE;
+        unassignedSectors -= NumDirect;
+
+        int j = 0;
+        int index[NumIndirect];
+        while (unassignedSectors != 0){
+            index[j] = freeMap->Find();
+            if (index[j] == -1)
+                return FALSE;
+
+            int indexSectors = unassignedSectors > NumIndirect ? NumIndirect : unassignedSectors;
+            int sectors[NumIndirect];
+            if (freeMap->NumClear() < indexSectors)
+                return FALSE;		
+            for (int i = 0; i < indexSectors; i++)
+                sectors[i] = freeMap->Find();
+            synchDisk->WriteSector(index[j], (char *)sectors);
+            unassignedSectors -= indexSectors;
+            j++;
+            if (j == NumIndirect)
+                return FALSE;
+        }
+        synchDisk->WriteSector(dataSectors[NumDirect], (char *) index);
+    }
+    else {
+        if (freeMap->NumClear() < unassignedSectors)
+            return FALSE;		// not enough space
+        for (int i = 0; i < unassignedSectors; i++)
+            dataSectors[i] = freeMap->Find();
+    }
+    return TRUE;
+}
+
+//----------------------------------------------------------------------
+// FileHeader::Deallocate
+// 	De-allocate all the space allocated for data blocks for this file.
+//
+//	"freeMap" is the bit map of free disk sectors
+//----------------------------------------------------------------------
+
+void 
+FileHeader::Deallocate(BitMap *freeMap)
+{
+    if (numSectors > NumDirect) {
+        int UnclearedSectors = numSectors;
+        for (int i = 0; i < NumDirect; i++) {
+            ASSERT(freeMap->Test((int) dataSectors[i]));  // ought to be marked!
+            freeMap->Clear((int) dataSectors[i]);
+        }
+        UnclearedSectors -= NumDirect;
+
+        int index[NumIndirect];
+        synchDisk->ReadSector(dataSectors[NumDirect], (char *)index);
+        int j = 0;
+        while (UnclearedSectors != 0) {
+            int indexSectors = UnclearedSectors > NumIndirect ? NumIndirect : UnclearedSectors;
+            int sectors[NumIndirect];
+            synchDisk->ReadSector(index[j], (char *)sectors);
+            for (int i = 0; i < indexSectors; i++) {
+                ASSERT(freeMap->Test((int) sectors[i]));  // ought to be marked!
+                freeMap->Clear((int) sectors[i]);
+            }
+            ASSERT(freeMap->Test((int) index[j]));
+            freeMap->Clear((int) index[j]);
+            UnclearedSectors -= indexSectors;
+            j++;
+        }
+    }
+    else {
+        for (int i = 0; i < numSectors; i++) {
+            ASSERT(freeMap->Test((int) dataSectors[i]));  // ought to be marked!
+            freeMap->Clear((int) dataSectors[i]);
+        }
+    }
+}
+
+//----------------------------------------------------------------------
+// FileHeader::FetchFrom
+// 	Fetch contents of file header from disk. 
+//
+//	"sector" is the disk sector containing the file header
+//----------------------------------------------------------------------
+
+void
+FileHeader::FetchFrom(int sector)
+{
+    synchDisk->ReadSector(sector, (char *)this);
+}
+
+//----------------------------------------------------------------------
+// FileHeader::WriteBack
+// 	Write the modified contents of the file header back to disk. 
+//
+//	"sector" is the disk sector to contain the file header
+//----------------------------------------------------------------------
+
+void
+FileHeader::WriteBack(int sector)
+{
+    synchDisk->WriteSector(sector, (char *)this); 
+}
+
+//----------------------------------------------------------------------
+// FileHeader::ByteToSector
+// 	Return which disk sector is storing a particular byte within the file.
+//      This is essentially a translation from a virtual address (the
+//	offset in the file) to a physical address (the sector where the
+//	data at the offset is stored).
+//
+//	"offset" is the location within the file of the byte in question
+//----------------------------------------------------------------------
+
+int
+FileHeader::ByteToSector(int offset)
+{
+    int flag = (offset == 896);
+    int sizeDirectIndex = NumDirect * SectorSize;
+    int sizePerIndex = NumIndirect * SectorSize;
+    if (offset >= sizeDirectIndex) {
+        offset -= sizeDirectIndex;
+        int index[NumIndirect];
+        synchDisk->ReadSector(dataSectors[NumDirect], (char *)index);
+        int indexSector = index[offset / sizePerIndex];
+        int sectors[NumIndirect];
+        synchDisk->ReadSector(indexSector, (char *)sectors);
+        return(sectors[(offset % sizePerIndex) / SectorSize]);
+    }
+    else
+        return(dataSectors[offset / SectorSize]);
+}
+
+//----------------------------------------------------------------------
+// FileHeader::FileLength
+// 	Return the number of bytes in the file.
+//----------------------------------------------------------------------
+
+int
+FileHeader::FileLength()
+{
+    return numBytes;
+}
+
+//----------------------------------------------------------------------
+// FileHeader::Print
+// 	Print the contents of the file header, and the contents of all
+//	the data blocks pointed to by the file header.
+//----------------------------------------------------------------------
+
+void
+FileHeader::Print()
+{
+    int i, j, k;
+    char *data = new char[SectorSize];
+    char *typeName[5] = {"Normal File", "Directory File", "BitMap File", "Name File", "Path File"};
+    int AllDataSectors[numSectors];
+
+    printf("--------------- FileHeader contents Print ---------------\n");
+    printf("File Type: %s\n", typeName[int(type)]);
+    if (type == NormalFile || type == DirectoryFile) {
+        printf("Create Time: %s\n", createTime);
+        printf("Last Access Time: %s\n", lastAccessTime);
+        printf("Last Modify Time: %s\n", lastModifyTime);
+        char *path = new char[pathLength]; 
+        getPath(path);
+        printf("Path: %s\n", path);
+    }
+    printf("File size: %d\nFile blocks:\n", numBytes);
+    k = 0;
+    if (numSectors < NumDirect)
+        for (i = 0; i < numSectors; i++) {
+            AllDataSectors[k++] = dataSectors[i];
+            printf("%d ", dataSectors[i]);
+        }
+    else {
+        int UnprintedSectors = numSectors;
+        for (i = 0; i < NumDirect; i++) {
+            AllDataSectors[k++] = dataSectors[i];
+            printf("%d ", dataSectors[i]);
+        }
+        UnprintedSectors -= NumDirect;
+        int index[NumIndirect];
+        synchDisk->ReadSector(dataSectors[NumDirect], (char *)index);
+        j = 0;
+        while (UnprintedSectors != 0) {
+            int sectors[NumIndirect];
+            synchDisk->ReadSector(index[j], (char *)sectors);
+            int indexSectors = UnprintedSectors > NumIndirect ? NumIndirect : UnprintedSectors;
+            for (i = 0; i < indexSectors; i++) {
+                AllDataSectors[k++] = sectors[i];
+                printf("%d ", sectors[i]);
+            }
+            UnprintedSectors -= indexSectors;
+            j++;
+        }
+    }
+    printf("\nFile contents:\n");
+    for (i = k = 0; i < numSectors; i++) {
+        synchDisk->ReadSector(AllDataSectors[i], data);
+        for (j = 0; (j < SectorSize) && (k < numBytes); j++, k++) {
+            if ('\040' <= data[j] && data[j] <= '\176')   // isprint(data[j])
+                printf("%c", data[j]);
+            else
+                printf("\\%x", (unsigned char)data[j]);
+        }
+        printf("\n"); 
+    }
+    printf("\n"); 
+    delete [] data;
+}
+
+void
+FileHeader::setCreateTime()
+{
+    time_t _time;
+    time(&_time);
+    strncpy(createTime, asctime(gmtime(&_time)), TimeLength);
+    createTime[TimeLength] = '\0';
+    DEBUG('f', "Create file in %s\n", createTime);
+}
+
+void
+FileHeader::setLastAccessTime()
+{
+    time_t _time;
+    time(&_time);
+    strncpy(lastAccessTime, asctime(gmtime(&_time)), TimeLength);
+    lastAccessTime[TimeLength] = '\0';
+    DEBUG('f', "Access file in %s\n", lastAccessTime);
+}
+
+void
+FileHeader::setLastModifyTime()
+{
+    time_t _time;
+    time(&_time);
+    strncpy(lastModifyTime, asctime(gmtime(&_time)), TimeLength);
+    lastModifyTime[TimeLength] = '\0';
+    DEBUG('f', "Modify file in %s\n", lastModifyTime);
+}
+
+void
+FileHeader::getPath(char *into)
+{
+    OpenFile *pathFile = new OpenFile(pathFileSector);
+    pathFile->ReadAt(into, pathLength, 0);
+    delete pathFile;
+}
